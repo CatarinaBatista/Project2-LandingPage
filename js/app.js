e02c887c11d3d0aa4df74d95061490f7746f6cc2
Catarina Batista <catarinabatistayou@gmail.com>
2019-11-07 08:57:57 +0000

    structure

---
+++ js/app.js
@@ -0,0 +1,13 @@
+/* Function to create navbar items */
+function createNavItems() {
+    
+}
+
+createNavItems();
+
+/* Function to create header elements */
+function createHeader() {
+    
+}
+
+createHeader();
