cb6b7d89d8c1b2acb0b4f7a6a9e42c9f5dc922be
Catarina Batista <catarinabatistayou@gmail.com>
2019-11-08 18:00:48 +0000

    sections created

---
+++ README.md
@@ -0,0 +1,8 @@
+# Project 2 - Landing Page from Front End Developer Nanodegree
+
+## About
+This project is an example of a landing page about 4 differents smartwatchs.
+
+## How to use 
+In this project, you can navigate to the information of a smartwatch throught navBar that it's going to lead you to the selected smartwatch.
+You can easily go back to the top of the page by clicking the button located in the bottom right corner of the page.
--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-/* global variables */
+/* Global variables */
 const navBar = document.getElementById("items-nav");
+
 
 /* Function to create navbar items */
 function createNavItems() {
@@ -19,27 +20,97 @@ function createNavItems() {
 createNavItems();
 
 
-/* Function to create header elements */
+/* Create header elements */
 function createHeader() {
     const header = document.getElementById('header');
 
-    /* create a title and a paragraph and add to header */
+    /* create a title, 2 paragraphs and add to header */
 
     const title = document.createElement('h1');
-    title.textContent = 'What is a smartWatch?';
+    title.textContent = 'What is a smartwatch?';
 
     const paragraph = document.createElement('p');
-    paragraph.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue.';
+    paragraph.textContent = 'A smartwatch is a wearable computing device marketed as more than a cell phone. ' +
+        'It is more than a simple watch, they use touchscreens, offer apps, and often record your heart rate and other vital signs.';
+    
+    const paragraph2 = document.createElement('p');
+    paragraph2.textContent = 'A smartwatch is also known as a smart wristwatch.';
 
-    header.appendChild(title)
-    header.appendChild(paragraph)
+    header.appendChild(title);
+    header.appendChild(paragraph);
+    header.appendChild(paragraph2);
 
     /* style header */
-    header.setAttribute('style', 'grid-area: 1/1/2/3; display: flex; flex-direction: column; justify-content: center; text-align: center; padding: 0 50px');
+    header.setAttribute(
+        'style', 
+        'display: flex; flex-direction: column; justify-content: center; text-align: center;'
+    );
 
 };
 
 createHeader();
+
+
+/* Create 4 sections about smartwatchs */
+function createSections() {
+
+    /* array of objects to save the data of each smartwatch */
+    const doc = [
+        {
+            info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt.',
+            image: './images/apple.png'
+        },
+        {
+            info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt.',
+            image: './images/ticwatch.png'
+        },
+        {
+            info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt.',
+            image: './images/xiaomi.png'
+        },
+        {
+            info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt.',
+            image: './images/samsung.png'
+        }
+    ];
+    
+    /* save all navBar links */
+    const navItems = document.querySelectorAll("a.links");
+
+    /* Go through navItems */
+    navItems.forEach((item, index) => {        
+        const section = document.querySelector(item.getAttribute('href'));
+        const watchInfo = doc[index];
+
+        /* create a div (to be styled) and add an image  */
+        const div = document.createElement('div');
+
+        const img = document.createElement('img');
+        img.src = watchInfo.image;
+        img.alt = section.getAttribute('data-nav');
+
+        div.appendChild(img)
+
+        /* create an article and add a title and a paragraph */
+        const article = document.createElement('article');
+
+        const title = document.createElement('h2');
+        title.textContent = section.getAttribute('data-nav');
+        
+        const info = document.createElement('p');
+        info.textContent = watchInfo.info;
+        
+        article.appendChild(title)    
+        article.appendChild(info)
+        
+        /* add div and  article to section */
+        section.appendChild(div);
+        section.appendChild(article);
+    });
+};
+
+createSections();
+
 
 
 /* scroll to top when button (id = btn-scroll-top) is clicked */
@@ -51,7 +122,7 @@ document.getElementById("btn-scroll-top").addEventListener('click', function() {
 /* Show or hide button (id = btn-scroll-top) */
 const showHideButton = () => {
     const btn = document.getElementById("btn-scroll-top");
-    let showButton = window.scrollY > window.innerHeight / 3;
+    const showButton = window.scrollY > window.innerHeight / 3;
 
     if (showButton === true) {
         btn.style.visibility = "visible";
@@ -62,3 +133,61 @@ const showHideButton = () => {
 
 document.addEventListener("scroll", showHideButton);
 
+
+/* 
+https://stackoverflow.com/questions/1350581/how-to-get-an-elements-top-position-relative-to-the-browsers-viewport?noredirect=1&lq=1
+
+const activateMenuForMostVisibleSection = () => {
+    const indexOfMinBounding = getIndexOfMinPositiveBounding([...navItems]);
+  
+    // knowing index of minimum positive bounding value we are able to decide which menu item to activate
+    [...navItems].map((menu, index) => {
+      if (index === indexOfMinBounding) {
+        menu.classList.add("active");
+      } else {
+        menu.classList.remove("active");
+      }
+    })
+  };
+  
+const getIndexOfMinPositiveBounding = (sectionsArray) => {
+    // create a "bounding values" array representing top bounding values for all our sections
+    const boundingArray = sectionsArray.map( section => {
+      return section.getBoundingClientRect().top;
+    });
+  
+    // find the minimum positive bounding value
+    const minBounding = boundingArray.reduce((acc, item) => item < acc && item > 0 ? item : acc, Number.MAX_SAFE_INTEGER);
+  
+    // return index of minimum positive bounding to know which menu item we should make active
+    return boundingArray.findIndex(item => item === minBounding);
+};
+
+
+// 
+var rect = elem.getBoundingClientRect();
+var win = elem.ownerDocument.defaultView;
+
+return {
+    top: rect.top + win.pageYOffset,
+    left: rect.left + win.pageXOffset
+};
+
+
+//teste
+const activateMenuForMostVisibleSection = () => {
+    
+    const navItems = document.querySelectorAll("a.links");
+
+    navItems.forEach(element => {
+        var rect = element.getBoundingClientRect();
+        var win = element.ownerDocument.defaultView;
+
+        console.log(rect.top + '     ' + win.pageYOffset)
+    });
+};
+
+
+document.addEventListener("scroll", activateMenuForMostVisibleSection)
+
+*/
